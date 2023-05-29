85a4e359dbe89134715e7421ccf1d7d05a0b7115
Matteo Raso <matteo_luigi_raso@protonmail.com>
2023-05-28 23:13:39 -0400

    Added license

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 This classifier achieves a mean accuracy of 90.7% by using gradient
 boosted trees.
 
-#Disclaimer
+# License
+
+This work is licensed under the Apache v2.
+
+# Disclaimer
 
 This is not meant to be used as a replacement for medical advice. See
 a doctor if you suspect that you're having heart problems.
--- classification.py
+++ classification.py
@@ -1,3 +1,19 @@
+'''
+Copyright 2023 Matteo Raso
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
 import numpy as np
 import pandas as pd
 import pickle as pk
@@ -20,9 +36,7 @@ validation_variables = variables[split:, :]
 
 training_target = target[:split]
 validation_target = target[split:]
-'''
-classifier = SVC()
-'''
+
 classifier = GradientBoostingClassifier()
 classifier.fit(training_variables, training_target)
 accuracy = classifier.score(validation_variables, validation_target)
